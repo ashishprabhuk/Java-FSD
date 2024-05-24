c6c5bc2208ad2cccfba6d5bec6a74a297f4e5a83
Ashish Prabhu K <ashishprabhu2143@gmail.com>
2024-05-24 19:37:50 +0530

    updates

--- Basics/StrBuilder.java
+++ Basics/StrBuilder.java
@@ -2,7 +2,41 @@ package Basics;
 
 public class StrBuilder {
     public static void main(String[] args) {
-        int a = 5;                           
-        System.out.println(a++ + ++a - ++a - a--);
+        // StringBuilder
+        StringBuilder sb = new StringBuilder("Hello");
+        System.out.println("StringBuilder: " + sb);
+
+        // Append
+        sb.append(" World");
+        System.out.println("Append: " + sb);
+
+        // Insert
+        sb.insert(5, "Java ");
+        System.out.println("Insert: " + sb);
+
+        // Delete
+        sb.delete(5, 10);
+        System.out.println("Delete: " + sb);
+
+        // Reverse
+        sb.reverse();
+        System.out.println("Reverse: " + sb);
+
+        // Length
+        System.out.println("Length: " + sb.length());
+
+        // Capacity
+        System.out.println("Capacity: " + sb.capacity());
+
+        // Replace
+        sb.replace(5, 10, "Java");
+        System.out.println("Replace: " + sb);
+
+        // Substring
+        System.out.println("Substring: " + sb.substring(5, 9));
+
+        // Trim
+        StringBuilder sb2 = new StringBuilder("   Hello World   ");
+        System.out.println("Trim: " + sb2.toString().trim());
     }
 }
